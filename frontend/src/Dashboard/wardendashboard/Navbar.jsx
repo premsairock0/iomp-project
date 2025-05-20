91852a58484aa9d2ed3aa0924ba943649bbea480
KuttumuRohit <rohitkuttum321@gmail.com>
2025-05-20 17:48:49 +0530

    yes

--- frontend/src/Dashboard/wardendashboard/Navbar.jsx
+++ frontend/src/Dashboard/wardendashboard/Navbar.jsx
@@ -61,7 +61,7 @@ function Navbar({ role }) {
                 onClick={handleLogout}
                 className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
               >
-                Yes, Log Out
+                Yes
               </button>
             </div>
           </div>
